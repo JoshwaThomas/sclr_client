7777f1361cf4d2920358a7e0da4b737e054f9981
Joshwa <joshwajospin2003@gmail.com>
2024-06-02 16:07:46 +0530

    firstpage

--- src/components/start/first.js
+++ src/components/start/first.js
@@ -5,8 +5,9 @@ function LandingPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-center items-center bg-yellow-300 container">
-      <div className="space-x-4">
+    <div className=" w-screen h-screen flex flex-col space-x-4  overflow-hidden justify-center items-center p-10 bg-gradient-to-br from-[#5decf1] to-[#b52af5]">
+    <div className="flex justify-center items-center rounded-lg bg-yellow-300 ">
+      <div className=' h-56 flex flex-col justify-evenly rounded-lg  mx-40'>
         <button
           onClick={() => navigate('/student')}
           className="px-4 py-2 bg-blue-500 text-white rounded-lg"
@@ -20,7 +21,9 @@ function LandingPage() {
           Admin
         </button>
       </div>
+      </div>
     </div>
+    
   );
 }
 
