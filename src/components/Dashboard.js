caa0b7005714019405a2075c8e3cefc45e7ac18d
Joshwa <joshwajospin2003@gmail.com>
2024-06-02 16:40:23 +0530

    dash

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BsPeopleFill } from "react-icons/bs";
+import { PiStudentFill } from "react-icons/pi";
 
 
 function dashboard() {
   return (
     <div>
+      <div className=' flex flex-row'>
+        <div className=' bg-white box-border h-32 w-44 p-4 border-4'>
+        <BsPeopleFill />
+          No. of Students
+        </div>
+        <div className=' bg-white box-border h-32 w-44 p-4 ml-16 border-4'>
+        <PiStudentFill />
+          No. of Students Apply
+        </div>
+      </div>
     </div>
   )
 }
